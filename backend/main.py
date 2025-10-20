76f062745ebc85008be57bec54e393956fbe8761
GabrielVilchis-215460 <al215460@alumnos.uacj.mx>
2025-10-19 20:25:24 -0600

    modify main.py

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
 #from fastapi.middleware.cors import CORSMiddleware
-from typing import Union
+#from typing import Union
+from .api import products
+from .database import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.include_router(products.router)
+#@app.get("/")
+#def read_root():
+    #return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q":q}
+#@app.get("/items/{item_id}")
+##   return {"item_id": item_id, "q":q}
